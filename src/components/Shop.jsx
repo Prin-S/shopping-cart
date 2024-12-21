6fc25c51482776b584196343d0704e0a7f001735
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-12-21 13:17:14 +0700

    Added data fetching and modified product cards.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,36 +1,78 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { Navbar } from './Navbar.jsx';
 
-function Product() {
+function Product({ details }) {
 
   return (
     <div className="box">
-      <h2>Product name</h2>
-      <div><img className="img" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" /></div>
+      <img className="img" src={details.image} />
+      <h2 className="product-title">{details.title}</h2>
       <div><label htmlFor="amount">Amount: <input id="amount" className="amount" type="number" name="amount" size="3" /></label></div>
+      <p>{`$${details.price}`}</p>
       <button className="button">Add to Cart</button>
     </div>
   );
 }
 
-function ProductBox() {
-  
+Product.propTypes = {
+  details: PropTypes.object,
+};
+
+function ProductBox({ items }) {
+
   return (
     <div className="container">
-      <Product />
-      <Product />
-      <Product />
+      {items.map(item => {
+        return <Product key={item.id} details={item} />
+      })}
     </div>
   );
 }
 
+ProductBox.propTypes = {
+  items: PropTypes.array,
+};
+
 function Shop() {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products', {mode: 'cors'})
+      .then(response => response.json())
+      .then(response => setItems(response))
+      .catch(error => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <Navbar />
+        <h1>Shop</h1>
+        <p>Loading...</p>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <Navbar />
+        <h1>Shop</h1>
+        <p>An error has occurred.</p>
+        <p>{`${error}`}</p>
+      </>
+    );
+  }
 
   return (
     <>
       <Navbar />
       <h1>Shop</h1>
-      <ProductBox />
+      <ProductBox items={items} />
     </>
   );
 }
